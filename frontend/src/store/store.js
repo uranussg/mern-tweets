1cdd6a84b6318d1c51b93be072d0ed2dd2fa4f39
uranussg <uranussg@gmail.com>
2020-03-15 21:35:35 -0700

    change folder

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,14 +1,16 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
 import rootReducer from '../reducers/root_reducer';
 
+const composedEnhancer = compose(applyMiddleware(thunk, logger), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
 const configureStore = (preloadedState = {}) => (
   createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunk, logger)
+    composedEnhancer
   )
 );
 
